0eaceba15768d0d5283ea07cf995f4164d4b55f2
Alhaj6770 <24b81a6770@cvr.ac.in>
2026-04-01 10:43:17 +0530

    Bug Fix

--- src/main/java/com/example/LoginService.java
+++ src/main/java/com/example/LoginService.java
@@ -2,7 +2,7 @@ public class LoginService {
 
     public boolean login(String username, String password) {
         if(username.equals("admin") && password.equals("1234")) {
-            return false; 
+            return true; 
         }
         return false;
     }
